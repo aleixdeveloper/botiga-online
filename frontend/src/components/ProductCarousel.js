95b39e39cdaded35cbe176c2d741975a5ba05940
aleixdeveloper <aleixclemente@gmail.com>
2020-12-16 23:49:33 +0100

    more carousel changes

--- frontend/src/components/ProductCarousel.js
+++ frontend/src/components/ProductCarousel.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 import { Carousel, Image } from 'react-bootstrap'
 import { useDispatch, useSelector } from 'react-redux'
@@ -12,20 +12,59 @@ const ProductCarousel = () => {
 
     const productTopRated = useSelector((state) => state.productTopRated)
     const { loading, error, products} = productTopRated
+    const randomNumber = Math.floor(Math.random()*7);
+    const [color,setColor] = useState({
+        background: 'linear-gradient(20deg, #167ddd 30%, #12e3ff 90%)'
+    })
+    
+    const colors = [
+        {
+            background: 'linear-gradient(20deg, #167ddd 30%, #12e3ff 90%)'
+        },
+        {
+            background: 'linear-gradient(20deg, #11998e 30%, #38ef7d 90%)'
 
+        },
+        {
+            background: 'linear-gradient(20deg, #23074d 30%, #cc5333 90%)'
+
+        },
+        {
+            background: 'linear-gradient(to right, #1a2a6c, #b21f1f, #fdbb2d)'
+
+        },
+        {
+            background: 'linear-gradient(20deg, #000046 30%, #1cb5e0 90%)'
+
+        },
+        {
+            background: 'linear-gradient(20deg, #80d0c7 30%, #13547a 90%)'
+
+        },
+        {
+            background: 'linear-gradient(to right, #5433ff, #20bdff, #a5fecb)'
+
+        },
+
+
+        
+    ]
     useEffect(() => {
         dispatch(listTopProducts())
+        setColor(colors[randomNumber])
+        console.log(randomNumber)
     },[dispatch])
+
 
     return loading ? (<Loader />) : error ? (<Message variant='danger'>{error}</Message>) : (
             <div className='hide-carousel' style={{marginBottom: '4em'}}>
-                <Carousel pause='hover' className='bg-dark carousel-background'>
+                <Carousel pause='hover' className='bg-dark carousel-background' style={color}>
                 {products.map((product) => (
                     <Carousel.Item key={product._id}>
                         <Link to={`/product/${product._id}`}>
                             <Image src={product.image} alt={product.name} />
                             <Carousel.Caption className='carousel-caption'>
-                                <h2>{product.name} ({product.price}€)</h2>
+                                <h3 style={{color: 'white'}}>{product.name} ({product.price}€)</h3>
                             </Carousel.Caption>
                         </Link>
                     </Carousel.Item>
